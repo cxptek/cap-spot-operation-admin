3aab197f99ea85fd6ed70623c2e1d27d218efd68
teedinh <tien.dq@cxptek.com>
2024-11-24 21:24:11 +0700

    add missing data

--- src/main/resources/db/common/migration/V1000_00__admin_init.sql
+++ src/main/resources/db/common/migration/V1000_00__admin_init.sql
@@ -11,3 +11,15 @@ CREATE TABLE trading_pair (
       taker_fee DECIMAL(38, 18) DEFAULT 0,
       PRIMARY KEY (id)
 );
+
+CREATE TABLE server_config (
+       id BIGINT AUTO_INCREMENT NOT NULL,
+       server_name VARCHAR(64) NOT NULL,
+       symbol_code VARCHAR(32) NOT NULL,
+       created_at TIMESTAMP NULL DEFAULT NULL,
+       updated_at TIMESTAMP NULL DEFAULT NULL,
+       created_by VARCHAR(32) DEFAULT NULL,
+       updated_by VARCHAR(32) DEFAULT NULL,
+       PRIMARY KEY (id)
+);
+
--- src/main/resources/db/common/migration/V2000_00__init_data.sql
+++ src/main/resources/db/common/migration/V2000_00__init_data.sql
@@ -5,4 +5,9 @@ VALUES
  (1,'DOT_VIC',true,'2024-06-11 17:15:58.344','2024-06-11 17:15:58.344','Admin','Admin',0.000100000000000000,0.000500000000000000),
  (1,'SHIB_VIC',true,'2024-06-11 17:15:58.344','2024-06-11 17:15:58.344','Admin','Admin',0.000100000000000000,0.000500000000000000);
 
-	
+
+INSERT INTO server_config (symbol_code,server_name,created_at,updated_at,created_by,updated_by) VALUES
+('USDT_KDG','server_write_side','2024-08-30 10:51:13.976','2024-08-30 10:51:13.976','Admin','Admin'),
+('TON_VIC','server_write_side','2024-08-30 10:51:13.976','2024-08-30 10:51:13.976','Admin','Admin'),
+('DOT_VIC','server_write_side','2024-08-30 10:51:13.976','2024-08-30 10:51:13.976','Admin','Admin'),
+('SHIB_VIC','server_write_side','2024-08-30 10:51:13.976','2024-08-30 10:51:13.976','Admin','Admin');
